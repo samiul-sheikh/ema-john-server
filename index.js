188b815ac055a72e858201545f853890d4b59f1a
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-30 05:56:50 +0600

    connect database and create post endpoint

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2uohe.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
 const port = 8080;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("emaJohnStore").collection("products");
+    console.log("database connected");
+
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        productsCollection.insertOne(product)
+            .then(result => {
+                console.log(result);
+            })
+    })
+});
+
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
 app.get('/', (req, res) => {
     res.send('welcome to ema-john server!')
